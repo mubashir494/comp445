a042ac73a583a9b46b2fb5500010e6527631acbc
Muhammad Mubashir <mubashirqbl@gmail.com>
2023-11-23 17:12:27 -0500

    sadas

--- Lab5-part1 (1)/fc/client.py
+++ Lab5-part1 (1)/fc/client.py
@@ -21,8 +21,8 @@ def main():
 
     sender = swp.SWPSender((settings.hostname, settings.port), 
             settings.loss_probability)
-    f = open("input.txt",'r')
-    for line in f.readlines():
+   
+    for line in sys.stdin:
         sender.send(line.encode())
 
 if __name__ == '__main__':

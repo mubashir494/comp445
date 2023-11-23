e2bc2fc4e603aebdb4455d0db5e9cc017e8aacc4
Muhammad Mubashir <mubashirqbl@gmail.com>
2023-11-23 17:27:30 -0500

    sadas

--- Lab5-part1 (1)/fc/swp.py
+++ Lab5-part1 (1)/fc/swp.py
@@ -98,7 +98,7 @@ class SWPSender:
         
         self.threads.append([packet.seq_num,timer])
         
-        sleep(0.5)
+        
         
         logging.debug("Threads Array After Appending "+str(self.threads))
     
@@ -106,7 +106,7 @@ class SWPSender:
         self._llp_endpoint.send(packet.to_bytes())
         
         # Start The retransmission thread
-        timer.start()
+        ##timer.start()
         
         # Increment the Sequence number
         self.sequence_number = self.sequence_number + 1

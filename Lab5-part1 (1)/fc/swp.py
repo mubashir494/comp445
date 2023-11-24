77cea11bea78cd7a2cd9aa81d945e0237d44f88f
Muhammad Mubashir <mubashirqbl@gmail.com>
2023-11-23 22:30:20 -0500

    asd

--- Lab5-part1 (1)/fc/swp.py
+++ Lab5-part1 (1)/fc/swp.py
@@ -267,14 +267,9 @@ class SWPReceiver:
                 
                 # Send Acknowledgment of Highest Acknowledged Segment
                 # If Sequence number is 0 then send zero as acknowledgment
-                if(self.expected_seq_num == -1):
-                    logging.debug("Sending ACK "+self.expected_seq_num +1)
-                    pack = SWPPacket(SWPType.ACK,self.expected_seq_num + 1)         
-                    self.ack_packet(pack)
-                else:
-                    logging.debug("Sending ACK "+self.expected_seq_num)
-                    pack = SWPPacket(SWPType.ACK,self.expected_seq_num)         
-                    self.ack_packet(pack)
+                logging.debug("Sending ACK "+self.expected_seq_num + 1)
+                pack = SWPPacket(SWPType.ACK,self.expected_seq_num + 1)         
+                self.ack_packet(pack)
                     
                                                 
             # TODO
